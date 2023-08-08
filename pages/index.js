f1675c20c74267c1b49ac0ba36c9ccb23db43e3d
Memo <mohammedfouadhassen@gmail.com>
2023-08-08 23:25:19 +0800

    demo page

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>COMMING SOON</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Mekaniku!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Empowering Car Owners in Malaysia via affordable quality spare parts.
         </p>
       </main>
 
